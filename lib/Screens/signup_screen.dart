ff22f4e5532855a6db9796422c42cbfaac84657a
Dipendra-Raghav <raghavdip19@gmail.com>
2023-07-03 15:43:39 +0530

    Inital commit , with half of authentication and basic login & signup

---
+++ lib/Screens/signup_screen.dart
@@ -0,0 +1,119 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:petterav1/resources/auth_methods.dart';
+
+import '../Widgets/text_field.dart';
+import '../utils/utils.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  Uint8List? _image;
+  bool _isLoading = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameController.dispose();
+  }
+
+  selectImage() async {
+    Uint8List im = await pickImage(ImageSource.gallery);
+    // set state because we need to display the image we selected on the circle avatar
+    setState(() {
+      _image = im;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 30),
+              Stack(
+                children: [
+                  _image != null
+                      ? CircleAvatar(
+                          radius: 64,
+                          backgroundImage: MemoryImage(_image!),
+                          backgroundColor: Colors.red,
+                        )
+                      : const CircleAvatar(
+                          radius: 64,
+                          backgroundImage: NetworkImage(
+                              'https://i.stack.imgur.com/l60Hf.png'),
+                          backgroundColor: Colors.red,
+                        ),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: selectImage,
+                      icon: const Icon(Icons.add_a_photo),
+                    ),
+                  )
+                ],
+              ),
+              TextFieldInput(
+                textEditingController: _usernameController,
+                hintText: 'Enter Your Username',
+                textInputType: TextInputType.text,
+              ),
+              TextFieldInput(
+                textEditingController: _emailController,
+                hintText: 'Enter Your Email',
+                textInputType: TextInputType.emailAddress,
+              ),
+              TextFieldInput(
+                textEditingController: _passwordController,
+                hintText: 'Enter Your Password',
+                textInputType: TextInputType.text,
+                isPass: true,
+              ),
+              TextFieldInput(
+                textEditingController: _bioController,
+                hintText: 'Enter Your bio',
+                textInputType: TextInputType.text,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  String res = await AuthMethods().signUpUser(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                    username: _usernameController.text,
+                    bio: _bioController.text,
+                  );
+                  print(res);
+                },
+                child: Text("SignUp"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Go to Sigin"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
