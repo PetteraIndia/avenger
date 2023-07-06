f6d023dc2e0d182511f06e3c6cafef90f95edc91
hariom <Hariom2013224@akgec.ac.in>
2023-07-06 15:25:21 +0530

    few edits

--- lib/Screens/newpost.dart
+++ lib/Screens/newpost.dart
@@ -38,7 +38,7 @@ class _newpostState extends State<newpost> {
     await FlutterImageCompress.compressAndGetFile(
       imagePath,
       compressedPath,
-      quality: 80, // Adjust the quality level (0 to 100)
+      quality: 90, // Adjust the quality level (0 to 100)
       rotate: 0, // Adjust the rotation angle (in degrees, 0 to 360)
     );
 
