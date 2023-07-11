1ab974fd564548ff2d287ecc8e3f88e37f667cbd
Dipendra-Raghav <raghavdip19@gmail.com>
2023-07-11 10:33:56 +0530

    loginscreen done , otp sigin done , boarding screen skipped for already users

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:petterav1/Screens/boarding_screen1.dart';
-import 'package:petterav1/Screens/login_screen.dart';
 import 'package:petterav1/Screens/newpost.dart';
 import 'package:petterav1/Screens/profile_screen.dart';
 import 'package:petterav1/Screens/socialmediapage.dart';
 import 'package:petterav1/mobileScreen.dart';
-import 'package:petterav1/resources/auth_service.dart';
 
-import 'Screens/boarding_screen3.dart';
+import 'Screens/LoginScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: AuthService().handleAuthState(),
+      home: LoginScreen(),
     );
   }
 }
